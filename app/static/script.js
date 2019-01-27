cf8e42ba7c52d0f88d9a9fa966df35f68d0582c2
Lyes HADJ AISSA <lyesha526@gmail.com>
2019-01-26 21:39:55 -0500

    Changing the index page

---
+++ app/static/script.js
@@ -0,0 +1,61 @@
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(45.497439, -73.578991),
+        zoom: 15,
+    };
+
+
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: { lat: 1222, lng: -43.578991 }
+    });
+    marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+function getPosition() {
+}
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+
+        var mapProp = {
+            center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+            zoom: 15,
+        };
+    
+    
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        marker = new google.maps.Marker({
+            map: map,
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            position: { lat: position.coords.latitude, lng: position.coords.longitude }
+        });
+        marker.addListener('click', toggleBounce);
+
+}
+
+
+
+
+
+
